1dd09abfb1d660ad35e86554578644eca9078cdb
md._.te <m.emami9@yahoo.com>
2018-05-27 22:45:00 +0430

    Location Picker Completed

--- app/src/main/java/ir/sq/apps/sqclubside/activities/FormActivity.java
+++ app/src/main/java/ir/sq/apps/sqclubside/activities/FormActivity.java
@@ -1,8 +1,10 @@
 package ir.sq.apps.sqclubside.activities;
 
 import android.content.Intent;
+import android.location.Address;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +12,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -63,7 +73,8 @@ public class FormActivity extends AppCompatActivity {
     TextInputLayout clubAddressInput;
 
     private EditText[] allEditTexts;
-
+    private GoogleApiClient mGoogleApiClient;
+    private int PLACE_PICKER_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,8 +82,29 @@ public class FormActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setFonts();
         setViews();
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .build();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
     }
 
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
     private void setViews() {
         allEditTexts = new EditText[5];
         allEditTexts[0] = clubNameEdittext;
@@ -107,16 +139,46 @@ public class FormActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.club_location_button:
-                startActivity(new Intent(FormActivity.this, ClubLocationActivity.class));
+//                startActivity(new Intent(FormActivity.this, ClubLocationActivity.class));
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(FormActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
                 break;
             case R.id.submit_information_button:
                 if (checkEmptyFields()) {
-                    sendUserToServer();
+//                    sendUserToServer();
                 }
                 break;
         }
     }
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                StringBuilder stBuilder = new StringBuilder();
+                String placename = String.format("%s", place.getName());
+                String latitude = String.valueOf(place.getLatLng().latitude);
+                String longitude = String.valueOf(place.getLatLng().longitude);
+                String address = String.format("%s", place.getAddress());
+                stBuilder.append("Name: ");
+                stBuilder.append(placename);
+                stBuilder.append("\n");
+                stBuilder.append("Latitude: ");
+                stBuilder.append(latitude);
+                stBuilder.append("\n");
+                stBuilder.append("Logitude: ");
+                stBuilder.append(longitude);
+                stBuilder.append("\n");
+                stBuilder.append("Address: ");
+                stBuilder.append(address);
+                Log.i("LOCATION", stBuilder.toString());
+            }
+        }
+    }
     private void sendUserToServer() {
         UserHandler.getInstance().createClub(allEditTexts[0].getText().toString(), clubOwnerEdittext.getText().toString(),
                 clubTelephonenumberEdittext.getText().toString(), clubCellphonenumberEdittext.getText().toString(), clubAddressEdittext.getText().toString());
